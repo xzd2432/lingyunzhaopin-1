3145a7dc4db936ca72903422be8587328ca95b27
sxh <s_xuhui@163.com>
2019-04-09 17:32:30 +0800

    上传api

---
+++ ly-service-api/src/test/java/com/lingyun/LyServiceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lingyun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyServiceApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
