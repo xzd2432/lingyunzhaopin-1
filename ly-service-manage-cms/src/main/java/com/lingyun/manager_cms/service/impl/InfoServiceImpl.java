e32580a4a3e752f26ae29df25ab0831ac2a2ceb8
zzr <1494700045@qq.com>
2019-04-10 16:46:51 +0800

    系统信息

---
+++ ly-service-manage-cms/src/main/java/com/lingyun/manager_cms/service/impl/InfoServiceImpl.java
@@ -0,0 +1,35 @@
+package com.lingyun.manager_cms.service.impl;
+
+import com.lingyun.framework.domain.admin.Info;
+import com.lingyun.manager_cms.dao.InfoMapper;
+import com.lingyun.manager_cms.service.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InfoServiceImpl implements InfoService{
+    @Autowired
+    private InfoMapper infoMapper;
+
+    @Override
+    public List<Info> query() {
+        return infoMapper.query();
+    }
+
+    @Override
+    public Info select(Integer infoid) {
+        return  infoMapper.selectByPrimaryKey(infoid);
+    }
+
+    @Override
+    public void del(Integer infoid) {
+        infoMapper.deleteByPrimaryKey(infoid);
+    }
+
+    @Override
+    public void delte(String ids) {
+        infoMapper.delete(ids);
+    }
+}
